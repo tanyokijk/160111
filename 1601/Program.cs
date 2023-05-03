f1354e160511b52133eb0c03dedef9396c863f4c
tdere <tdere@DESKTOP-K8RART3>
2023-05-03 21:56:38 +0300

    Добавьте файлы проекта.

---
+++ 1601/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace SimpleProject
+{
+    public class DllImportExample
+    {
+        [DllImport("User32.dll", ExactSpelling = true, CharSet = CharSet.Unicode)]
+        public static extern int MessageBoxW(IntPtr hWnd, string text, string caption, uint type);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DllImportExample.MessageBoxW(IntPtr.Zero, "Випробовування пройшло успішно", "Тест DllImportAttribute", 0);
+        }
+    }
+}
